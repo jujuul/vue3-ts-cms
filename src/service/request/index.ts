56a9300d570c01d15c5098c1f9b94e22cf2e8c90
jujuul <576583430@qq.com>
2022-03-06 14:23:01 +0800

    feat(wrap axios global interceptors): wrap axios global interceptors

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -10,6 +10,7 @@ class ZXRequest {
     this.instance = axios.create(config)
     this.interceptors = config.interceptors
 
+    // 从config中取出的是对应的实例的拦截器
     this.instance.interceptors.request.use(
       this.interceptors?.requestInterceptor,
       this.interceptors?.requestInterceptorCatch
@@ -18,6 +19,27 @@ class ZXRequest {
     this.instance.interceptors.response.use(
       this.interceptors?.responseInterceptor,
       this.interceptors?.responseInterceptorCatch
+    )
+
+    // 添加所有的实例都有的拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        console.log('全局实例请求拦截')
+        return config
+      },
+      (err) => {
+        return err
+      }
+    )
+
+    this.instance.interceptors.response.use(
+      (res) => {
+        console.log('全局实例响应拦截')
+        return res
+      },
+      (err) => {
+        return err
+      }
     )
   }
   request(config: ZXRequestConfig): void {
